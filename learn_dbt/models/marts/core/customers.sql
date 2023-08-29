6f66045c8b8b786ed056eb74bfb2be0228b461a4
Chrominux <paniaguadavidsantiago@gmail.com>
2023-08-29 07:31:15 +0000

    modified stuff

--- learn_dbt/models/marts/core/customers.sql
+++ learn_dbt/models/marts/core/customers.sql
@@ -5,7 +5,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_customers') }}
 
 ),
 
@@ -18,7 +18,7 @@ orders as (
         status,
         _etl_loaded_at
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_orders') }}
 
 ),
 
